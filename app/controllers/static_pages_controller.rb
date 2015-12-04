a8b5501f8dbadddea27e35f2fe1845aa06a83769
Laura Fitzgerald <laurafitzgeraldsemail@gmail.com>
2015-12-04 10:19:24 +0000

    Resolved issue with workout datamodel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   	if logged_in?
-  		@workouts = current_user.workout_runs.build
+  		@workouts = current_user.workouts.build
   		@feed_items = current_user.feed
   	end
   end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,5 @@
 class Workout < ActiveRecord::Base
 	belongs_to :user
 	#self.abstract_class = true
-  def self.table_name_prefix
-    'workout_'
-  end
+
 end
