4ede799993a742f2bd4d16d1ce3c05dd169c55a3
Laura Fitzgerald <laurafitzgeraldsemail@gmail.com>
2015-12-23 09:58:38 +0000

    Seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 user = User.create(name:'Joe Bloggs', email:'joebloggs@gmail.com', password: 'pass')
 
 
-workout = Workout.create(distance: '25', hours: '1', mins: '23', secs: '45' )
-workout.user = user
+user.workouts.create(distance: 25.5, hours: 1, mins: 23, secs: 45 )
+user.workouts.create(distance: 30.8, hours: 2, mins: 23, secs: 45 )
+user.workouts.create(distance: 56.2, hours: 5, mins: 23, secs: 45 )
+user.workouts.create(distance: 34.4, hours: 2, mins: 23, secs: 45 )
+user.workouts.create(distance: 25.4, hours: 1, mins: 23, secs: 45 )
+user.workouts.create(distance: 26.4, hours: 1, mins: 23, secs: 45 )
