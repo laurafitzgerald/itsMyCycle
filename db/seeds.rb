82a40f8662ab8a76623986bccb348359155dcf61
Laura Fitzgerald <laurafitzgeraldsemail@gmail.com>
2015-12-23 09:29:00 +0000

    Resolved issue with feed items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 user = User.create(name:'Joe Bloggs', email:'joebloggs@gmail.com', password: 'pass')
 
 
-user.workouts.create(distance: '25', hours: '1', mins: '23', secs: '45' )
+workout = Workout.create(distance: '25', hours: '1', mins: '23', secs: '45' )
+workout.user = user
