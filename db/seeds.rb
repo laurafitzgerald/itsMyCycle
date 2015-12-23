e5e3a7c4a486ef1590be043855a663fe87d4accd
Laura Fitzgerald <laurafitzgeraldsemail@gmail.com>
2015-12-23 09:07:33 +0000

    Fix feed issue error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-user = User.create()
+user = User.create(name:'Joe Bloggs', email:'joebloggs@gmail.com', password: 'pass')
+
+
+user.workouts.create(distance: '25', hours: '1', mins: '23', secs: '45' )
